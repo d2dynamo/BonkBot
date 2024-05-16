d6a768bd9133055c4833cbd1425ab74c4b10c0e8
unknown <herkusc@gmail.com>
2024-05-16 17:02:01 +0200

    added removedebt to commands index.

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -1,7 +1,8 @@
 import LoadDebt from "./loadDebt";
 import AddDebt from "./addDebt";
 import SetDebt from "./setDebt";
+import RemoveDebt from "./removeDebt";
 
-export { LoadDebt, AddDebt, SetDebt };
+export { LoadDebt, AddDebt, SetDebt, RemoveDebt };
 
-export default [LoadDebt, AddDebt, SetDebt];
+export default [LoadDebt, AddDebt, SetDebt, RemoveDebt];
