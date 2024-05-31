083676185ba58dc73a17e0e919d9caa8fc9fa388
unknown <herkusc@gmail.com>
2024-06-01 00:09:48 +0200

    test

--- src/main.ts
+++ src/main.ts
@@ -7,57 +7,61 @@ import Commands from "./commands";
 import registerUsers from "./modules/users/register";
 
 (async () => {
-  const client = new Client({
-    intents: [
-      GatewayIntentBits.GuildMembers,
-      GatewayIntentBits.Guilds,
-      GatewayIntentBits.GuildMessages,
-      GatewayIntentBits.MessageContent,
-    ],
-  });
-
-  if (!process.env.DBOT_TOKEN) {
-    console.error(">> No token provided");
-    return;
-  }
-
-  if (!process.env.SQLITE_DB_PATH) {
-    throw new Error("SQLITE_DB_PATH is not set.");
-  }
-
-  client.on(Events.ClientReady, async () => {
-    console.log(">> Bot started");
-
-    console.log(">> Registering commands");
-
-    client.application?.commands.set(
-      Commands.map((command) => command.data.toJSON())
-    );
-
-    await registerCommands();
-
-    const guilds = await client.guilds.fetch();
-    console.log(">> Fetched guilds", guilds.size);
-    guilds.forEach(async (guild) => {
-      console.log(">> Guild", guild.name);
-      const g = await guild.fetch();
-      await registerUsers(g);
+  try {
+    const client = new Client({
+      intents: [
+        GatewayIntentBits.GuildMembers,
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent,
+      ],
     });
 
-    console.log(">> Ready to bonk!");
-  });
-
-  client.on(Events.MessageCreate, async (message) =>
-    EventHandler.messageCreate(message)
-  );
-
-  client.on(Events.InteractionCreate, async (interaction) => {
-    if (interaction.isCommand() || interaction.isContextMenuCommand()) {
-      EventHandler.slashCommand(interaction);
+    if (!process.env.DBOT_TOKEN) {
+      console.error(">> No token provided");
       return;
     }
-    console.log(">> Interaction", interaction);
-  });
 
-  client.login(process.env.DBOT_TOKEN);
+    if (!process.env.SQLITE_DB_PATH) {
+      throw new Error("SQLITE_DB_PATH is not set.");
+    }
+
+    client.on(Events.ClientReady, async () => {
+      console.log(">> Bot started");
+
+      console.log(">> Registering commands");
+
+      client.application?.commands.set(
+        Commands.map((command) => command.data.toJSON())
+      );
+
+      await registerCommands();
+
+      const guilds = await client.guilds.fetch();
+      console.log(">> Fetched guilds", guilds.size);
+      guilds.forEach(async (guild) => {
+        console.log(">> Guild", guild.name);
+        const g = await guild.fetch();
+        await registerUsers(g);
+      });
+
+      console.log(">> Ready to bonk!");
+    });
+
+    client.on(Events.MessageCreate, async (message) =>
+      EventHandler.messageCreate(message)
+    );
+
+    client.on(Events.InteractionCreate, async (interaction) => {
+      if (interaction.isCommand() || interaction.isContextMenuCommand()) {
+        EventHandler.slashCommand(interaction);
+        return;
+      }
+      console.log(">> Interaction", interaction);
+    });
+
+    client.login(process.env.DBOT_TOKEN);
+  } catch (error) {
+    console.error(">> Main Func catch:", error);
+  }
 })();
