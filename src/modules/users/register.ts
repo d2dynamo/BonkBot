4e6ad2027d8d3c52872649f299bd426cb1359680
unknown <herkusc@gmail.com>
2024-06-17 02:07:42 +0200

    fix

--- src/modules/users/register.ts
+++ src/modules/users/register.ts
@@ -4,6 +4,8 @@ import parseUserId from "./userId";
 import createUser from "./create";
 import getUserWallet from "../debtWallet/get";
 import createWallet from "../debtWallet/create";
+import setUserPermission from "../../commands/permissions/setUserPermission";
+import { changeUserPermissions } from "./update";
 
 /**
  * Register users from a guild.
@@ -20,6 +22,11 @@ export default async function registerUsersFromGuild(guild: Guild) {
 
       try {
         await createUser(parseUserId(members[j].id), members[j].user.username);
+
+        await changeUserPermissions(parseUserId(members[j].id), {
+          permissionId: 1,
+          active: true,
+        });
 
         try {
           await getUserWallet(parseUserId(members[j].id));
